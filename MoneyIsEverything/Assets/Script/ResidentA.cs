24cf96b99b7a241bb54f1cf912dc23d46be843ae
Unknown <hakuya8zai@gmail.com>
2020-03-12 20:59:02 +0800

    Input NPCs

---
+++ MoneyIsEverything/Assets/Script/ResidentA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ResidentA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+        }
+    }
+}
