c7193992b6674116d31e1b3b3283d71aab76fdc2
Unknown <hakuya8zai@gmail.com>
2020-03-12 20:31:53 +0800

    Real Scene,Character,Smooth Camera Move

---
+++ MoneyIsEverything/Assets/SpriteGhostTrailRenderer/ActionCode2D/Scripts/Cameras/CameraLockFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace ActionCode2D.Cameras
+{
+    [DisallowMultipleComponent]
+    public sealed class CameraLockFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float smoothSpeed = 0.125f;
+        public Vector2 offset;
+
+        private void LateUpdate()
+        {
+            UpdatePosition();
+        }
+
+        private void OnValidate()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            if (target)
+            {
+                Vector2 desiredPosition =new Vector2(target.position.x,target.position.y) + offset;
+                Vector2 pos = Vector2.Lerp(transform.position,desiredPosition,smoothSpeed);
+                transform.position =new Vector3 (pos.x,pos.y,-20.0f);
+                
+            }
+        }
+    }
+}
